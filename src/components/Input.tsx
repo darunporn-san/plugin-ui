9113f7ae555340d32fe32514274d8127a14d0865
darunporn-san <darunpornsantisawaddiwong@gmail.com>
2025-07-31 14:38:48 +0700

    not

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -3,14 +3,7 @@ import type { BaseInputProps } from '../types';
 import { Input as InputShadcn} from '@/components/ui/input';
 
 export const Input: React.FC<BaseInputProps> = ({
-  label,
-  tooltip,
-  error,
-  required,
-  id,
-  placeholder,
-  disabled,
-  ...props
+  
 }) => {
   return (
     <div className="flex flex-col gap-1 w-full relative">
